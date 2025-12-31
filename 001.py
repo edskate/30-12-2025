eccdadd40cf3e275ed9ccbc88a4ac9fdc86300ca
Edy_97_2024 <edskateicevida@gmail.com>
2025-12-31 02:05:48 -0300

    atualização

---
+++ 001.py
@@ -0,0 +1,12 @@
+
+# Condições (if / else)
+
+nome = input ("Digite seu nome: ")
+print ("Olá, " + nome + "!")
+print ("Olá, {nome}!")
+print ("Olá, {}".format(nome))
+print ("Olá, %s!" % nome)
+print ("Olá, " + str(nome) + "!")
+print ("Olá, " , nome , "!")
+print ("Olá, " + nome.upper() + "!")
+print ("Olá, " + nome.lower() + "!")
